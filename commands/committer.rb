1dff5ce5b0af09023996bcc9e694364191f61f04
tracyatrf <tmeade@renewfinancial.com>
2018-01-17 20:47:25 -0800

    Create commits

--- commands/committer.rb
+++ commands/committer.rb
@@ -1,5 +1,11 @@
 class Committer
-  def run
-    puts TreeBuilder.create_trees(Index.load.files)
+  def run(message, *_args)
+    commit_content = {
+      tree: TreeBuilder.create_trees(Index.load.files)[:sha],
+      message: message
+    }
+    artifact = Artifact.create(type: :commit, raw_content: commit_content.to_yaml)
+    puts "Successfully created commit #{artifact.sha}"
+    true
   end
 end
